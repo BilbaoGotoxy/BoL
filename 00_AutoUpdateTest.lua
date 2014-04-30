7d217328a601736a72c1b4e59add397c5b8baf4d
G0t0xy <bilbao@gmx.ru>
2014-04-30 21:12:45 +0200

    Update 00_AutoUpdateTest.lua

--- 00_AutoUpdateTest.lua
+++ 00_AutoUpdateTest.lua
@@ -1,5 +1,7 @@
-local version = "0.02"
+local version = "1.0"
 --https://raw.githubusercontent.com/G0t0xy/BoL/master/00_AutoUpdateTest.lua
+
+
 local AUTOUPDATE = true
 local UPDATE_NAME = "00_AutoUpdateTest"
 local UPDATE_HOST = "raw.github.com"
@@ -8,22 +10,22 @@ local UPDATE_FILE_PATH = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
 local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
 function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>"..UPDATE_NAME..":</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
-if AUTOUPDATE then
-local ServerData = GetWebResult(UPDATE_HOST, UPDATE_PATH, "", 5)
-if ServerData then
-local ServerVersion = string.match(ServerData, "local version = \"%d+.%d+\"")
-ServerVersion = string.match(ServerVersion and ServerVersion or "", "%d+.%d+")
-if ServerVersion then
-ServerVersion = tonumber(ServerVersion)
-if tonumber(version) < ServerVersion then
-AutoupdaterMsg("New version available"..ServerVersion)
-AutoupdaterMsg("Updating, please don't press F9")
-DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end)	
-else
-AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
-end
-end
-else
-AutoupdaterMsg("Error downloading version info")
-end
-end
+	if AUTOUPDATE then
+		local ServerData = GetWebResult(UPDATE_HOST, UPDATE_PATH, "", 5)
+		if ServerData then
+			local ServerVersion = string.match(ServerData, "local version = \"%d+.%d+\"")
+			ServerVersion = string.match(ServerVersion and ServerVersion or "", "%d+.%d+")
+			if ServerVersion then
+				ServerVersion = tonumber(ServerVersion)
+				if tonumber(version) < ServerVersion then
+					AutoupdaterMsg("New version available "..ServerVersion)
+					AutoupdaterMsg("Updating, please don't press F9")
+					DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end)	
+				else
+					AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
+				end
+			end
+		else
+			AutoupdaterMsg("Error downloading version info")
+		end
+	end
