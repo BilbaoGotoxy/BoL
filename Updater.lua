212838c53082eff046fd1ffefa23a7e76e67c719
G0t0xy <bilbao@gmx.ru>
2014-04-30 21:51:23 +0200

    Update Updater.lua

--- Updater.lua
+++ Updater.lua
@@ -1,39 +1,33 @@
-local version = "0.05"
+local version = "0.01"
 --https://github.com/G0t0xy/BoL/blob/master/Updater.lua
 
 
-local AUTOUPDATE = true
-local UPDATE_NAME = "Updater"
+
+--Honda7
+local autoupdateenabled = true
+local UPDATE_NAME = "UpdateTest"
 local UPDATE_HOST = "raw.github.com"
-local UPDATE_PATH = "/G0t0xy/BoL/master/Updater.lua".."?rand="..math.random(1,10000)
+local UPDATE_PATH = "G0t0xy/BoL/blob/master/Updater.lua".."?rand="..math.random(1,10000)
 local UPDATE_FILE_PATH = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
-local UPDATE_URL = "http://"..UPDATE_HOST..UPDATE_PATH
+local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
 function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>"..UPDATE_NAME..":</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
-	if AUTOUPDATE then
-		local ServerData = GetWebResult(UPDATE_HOST, UPDATE_PATH, "", 5)
-		if ServerData then
-			local ServerVersion = string.match(ServerData, "local version = \"%d+.%d+\"")
-			
-			ServerVersion = string.match(ServerVersion and ServerVersion or "", "%d+.%d+")
-			if ServerVersion then				
-				ServerVersion = tonumber(ServerVersion)
-				
-				print("normal: "..ServerVersion)
-				print("string: "..tostring(ServerVision))
-				print("number: "..tonumber(ServerVersion))
-				print("local ver: "..version)
-				
-				
-				if tonumber(version) < ServerVersion then
-					AutoupdaterMsg("New version available "..ServerVersion)
-					AutoupdaterMsg("Updating, please don't press F9")
-					DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end)	
-				else
-					AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
-				end
-			end
-		else
-			AutoupdaterMsg("Error downloading version info")
-		end
+if AUTOUPDATE then
+    local ServerData = GetWebResult(UPDATE_HOST, UPDATE_PATH)
+    if ServerData then
+        local ServerVersion = string.match(ServerData, "local version = \"%d+.%d+\"")
+        ServerVersion = string.match(ServerVersion and ServerVersion or "", "%d+.%d+")
+        if ServerVersion then
+            ServerVersion = tonumber(ServerVersion)
+            if tonumber(version) < ServerVersion then
+                AutoupdaterMsg("New version available"..ServerVersion)
+                AutoupdaterMsg("Updating, please don't press F9")
+                DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
+            else
+                AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
+            end
+        end
+    else
+        AutoupdaterMsg("Error downloading version info")
+    end
 end
