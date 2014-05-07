a8694e231e898b060849bb19af5fc27cb785ccf1
BilbaoGotoxy <BilbaoGotoxy@users.noreply.github.com>
2014-05-07 02:10:37 +0200

    Update BilbaoThreshLaternClicker.lua
    v2

--- BilbaoThreshLaternClicker.lua
+++ BilbaoThreshLaternClicker.lua
@@ -1,4 +1,4 @@
-if myHero.charName == "Thresh" then return end
+if myHero.charName == "Thresh" or not VIP_USER then return end
 function OnLoad()
 	Lanternm = scriptConfig("Lantern Clicker", "Lantern")	
 		Lanternm:addSubMenu("OnKeyDown", "okdmenu")
@@ -10,17 +10,22 @@ function OnLoad()
 			Lanternm.onoffmenu:addParam("hconoff", "Use Lantern under % health", SCRIPT_PARAM_SLICE, 25, 0, 100, 0)
 			Lanternm.onoffmenu:permaShow("lanoffon")				
 		Lanternm:addSubMenu("ForceClickRange", "fcR")
-			Lanternm.fcR:addParam("fcrange", "Run to Lantern if near", SCRIPT_PARAM_SLICE, 100, 100, 1000, 0)			
-	PrintChat("<font color='#03dafb'> >> Lantern Clicker loaded. </font>")	
-	 Lanternm.okdmenu.UseLantern=false
+			Lanternm.fcR:addParam("fcrange", "Run to Lantern if near", SCRIPT_PARAM_SLICE, 100, 100, 1000, 0)		
+		Lanternm:addSubMenu("Drawing", "draw")
+			Lanternm.draw:addParam("lantern", "Mark Lantern Range", SCRIPT_PARAM_ONOFF, true)
+			Lanternm.draw:addParam("move", "Mark ForceRange", SCRIPT_PARAM_ONOFF, true)
+			Lanternm.draw:addParam("line", "Line to Lantern", SCRIPT_PARAM_ONOFF, true)
+	PrintChat("<font color='#03dafb'> >> Lantern Clicker by Bilbao loaded. </font>")	
+	Lanternm.okdmenu.UseLantern=false
 	Lanternm.onoffmenu.lanoffon =false
 end
 
 
-function Lantern(id)
+function Lantern()
+if not lant or not lant.valid or not (myHero:GetDistance(lant) < Lanternm.fcR.fcrange and lant.valid and lant) then return end
 	p = CLoLPacket(0x39)
 	p:EncodeF(myHero.networkID)
-	p:EncodeF(id)
+	p:EncodeF(lant.networkID)
 	p.dwArg1 = 1
 	p.dwArg2 = 0
 	SendPacket(p)
@@ -34,55 +39,17 @@ function OnCreateObj(obj)
 end
 
 
-function _clicklantern()
-	if lant and lant.valid and _inRangeToRun() then
-		Lantern(lant.networkID)
-	end
-end
-
 function OnDraw()
 	if lant and lant.valid then
-		DrawCircle(lant.x, lant.y, lant.z, 315, ARGB(100, 250, 0, 250))
-	end
-	if lant and lant.valid then
-		DrawCircle(lant.x, lant.y, lant.z, Lanternm.fcR.fcrange, ARGB(253,0, 253, 0))
-	end
-end
-
-
-function _inRangeToRun()
-	if  myHero:GetDistance(lant) < Lanternm.fcR.fcrange and lant.valid and lant then
-		return true
-	else
-		return false
-	end
-end
-
-
-function _iamlow_OKD()
-	if myHero.health <= (myHero.maxHealth*(Lanternm.okdmenu.okdhealth/ 100)) then
-		return true
-	else
-		return false
-	end
-end
-
-
-function _iamlow_ONOFF()
-	if myHero.health <= (myHero.maxHealth*(Lanternm.onoffmenu.hconoff/ 100)) then
-		return true
-	else
-		return false
+		if Lanternm.draw.lantern then DrawCircle(lant.x, lant.y, lant.z, 315, ARGB(100, 250, 0, 250)) end
+		if Lanternm.draw.move then DrawCircle(lant.x, lant.y, lant.z, Lanternm.fcR.fcrange, ARGB(253,0, 253, 0)) end
+		if Lanternm.draw.line then DrawLine3D(myHero.x, myHero.y, myHero.z, lant.x, lant.y, lant.z, 3, ARGB(100, 250, 0, 250)) end
 	end
 end
 
 
 function OnTick()
-if GetInGameTimer() < 120 then return end
-	if Lanternm.okdmenu.UseLantern and _iamlow_OKD() then
-		_clicklantern()
-	end	
-	if Lanternm.onoffmenu.lanoffon and _iamlow_ONOFF() then
-		_clicklantern()
-	end	
+	if (Lanternm.okdmenu.UseLantern and myHero.health <= (myHero.maxHealth*(Lanternm.okdmenu.okdhealth/ 100))) or (Lanternm.onoffmenu.lanoffon and myHero.maxHealth*(Lanternm.onoffmenu.hconoff/ 100)) then
+		Lantern()
+	end
 end
