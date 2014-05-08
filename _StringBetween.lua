60bd52ac78d07bb17db0dedfb286cab7c13161ea
BilbaoGotoxy <BilbaoGotoxy@users.noreply.github.com>
2014-05-08 15:22:29 +0200

    Update _StringBetween.lua

--- _StringBetween.lua
+++ _StringBetween.lua
@@ -1,6 +1,5 @@
 function _StringBetween(data, from, to)
 	local _, pos1 = data:find(from)
 	local pos2 = data:find(to)
-	local result = data:sub(pos1+1, pos2-1)
-	return result
+	return data:sub(pos1+1, pos2-1)
 end
