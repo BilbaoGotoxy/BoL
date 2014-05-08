75bf31262555cb9c784cfadeb0096b4546486056
BilbaoGotoxy <BilbaoGotoxy@users.noreply.github.com>
2014-05-08 15:23:47 +0200

    Update _StringBetween.lua

--- _StringBetween.lua
+++ _StringBetween.lua
@@ -1,3 +1,10 @@
+--[[
+Example:
+local data = "ABCDEF"
+local result = _StringBetween(data, "AB", "EF")
+The variable result will now have the value "CD".
+]]--
+
 function _StringBetween(data, from, to)
 	local _, pos1 = data:find(from)
 	local pos2 = data:find(to)
