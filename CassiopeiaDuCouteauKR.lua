a638c08b6893298c6fbf9e51a9155718497ed3ee
BilbaoGotoxy <BilbaoGotoxy@users.noreply.github.com>
2014-08-10 02:24:30 +0200

    Update CassiopeiaDuCouteauKR.lua

--- CassiopeiaDuCouteauKR.lua
+++ CassiopeiaDuCouteauKR.lua
@@ -1,1 +1,17 @@
-123
+if myHero.charName ~= "Cassiopeia" then return end
+--[[	---------------------------------       ]]--
+--[[		Cassiopeia Du CouteauKR		          		]]--
+--[[			by Bilbao					                 	]]--
+--[[	---------------------------------       ]]--
+
+
+--[[	----------------------------------------]]--
+--[[	Special Thanks to:		          				]]--
+--[[	Trees	 - for performance improvements  	]]--
+--[[	Klojke	 - good guy				          		]]--
+--[[	trustu  - translation			          		]]--
+--[[	----------------------------------------]]--
+
+--[[	Do not touch.	]]--
+_G.CassioVersion = 0.94
+--[[	-------------	]]--
